8590669ff6be636c7de34d1f04916b3df350353f
vaxo.sv <vaxo.sv@gmail.com>
2018-08-21 23:57:50 +0400

    init firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firabase: {
+    apiKey: "AIzaSyAlcjdCQWVzYg7377vFj9Jf48clvEgcfAQ",
+    authDomain: "fir-aa424.firebaseapp.com",
+    databaseURL: "https://fir-aa424.firebaseio.com",
+    projectId: "fir-aa424",
+    storageBucket: "fir-aa424.appspot.com",
+    messagingSenderId: "9785689179"
+  }
 };
 
 /*
